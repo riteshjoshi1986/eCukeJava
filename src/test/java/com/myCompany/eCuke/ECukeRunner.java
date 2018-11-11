f85a53b5d7f2ba1d9477656468cba50f4dea797d
Ritesh Joshi <riteshjoshi1986@gmail.com>
2018-11-11 17:39:48 +0530

    Adding to Test Git Hub

--- src/test/java/com/myCompany/eCuke/ECukeRunner.java
+++ src/test/java/com/myCompany/eCuke/ECukeRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+//Adding comments to test git functionality
 @CucumberOptions(
 		features = {"src/test/resources/com/myCompany/myProject/features/test.feature"},
 		//tags = {"@web","@wip","~@setup","~@regression","~@obsolete"}, // --> WIP feature
